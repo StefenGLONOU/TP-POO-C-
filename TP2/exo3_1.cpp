c7b8880b8842f7cb53af703dbe528114ebb8677c
StefenGLONOU <stefen-mawule.glonou@student.junia.com>
2025-09-15 17:17:06 +0200

    exo3

--- TP2/exo3_1.cpp
+++ TP2/exo3_1.cpp
@@ -1,25 +1,54 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <sstream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 int main(){
-	
-  ifstream file("test.txt");
-  if(!is_open(test.txt)){
-    cout << " Erreur, Impossible d'ouvrir le fichier" << endl;  
-  return 1; 
-  }
 
-  int nbLignes = 0, int nbLettres = 0, int nbMots = 0;
-  string ligne;
+    ifstream file("test.txt");
 
-  while(getline(file,ligne)){
-    nbLignes++;
+    if(!file.is_open()){
 
-  }
+      cerr << "Erreur : impossible d'ouvrir le fichier." << endl;
+      return 1;
+      
+    }
+    
+    int nbLignes = 0;
+    int nbMots = 0;
+    int nbLettres = 0;
+    string lignes;
 
-	return 0;
+    while(getline(file,lignes)){
+      nbLignes++;
+      stringstream ss(lignes); // Crée un flux à partir de la ligne
+      string mot;
+      
+      while( ss >> mot){
+        nbMots++;
+        for(char c : mot){
+            if(isalpha(c)){
+              nbLettres++;
+            }
+        }
+        
+
+      }
+
+    }
+
+    file.close();
+    
+    cout << "Etude du fichier" << endl;
+    cout << "Nombre de lignes : " << nbLignes << endl;
+    cout << "Nombre de mots : " << nbMots << endl;
+    cout << "Nombre de lettres : " << nbLettres << endl;
+
+
+
+
+
+  return 0;
 }
