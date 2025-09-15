3f84f63697522869f23154c4ae77158bb9c0e15a
StefenGLONOU <stefen-mawule.glonou@student.junia.com>
2025-09-15 09:47:12 +0200

    exo3

---
+++ TP2/exo3_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+	
+  ifstream file("test.txt");
+  if(!is_open(test.txt)){
+    cout << " Erreur, Impossible d'ouvrir le fichier" << endl;  
+  return 1; 
+  }
+
+  int nbLignes = 0, int nbLettres = 0, int nbMots = 0;
+  
+
+	return 0;
+}
